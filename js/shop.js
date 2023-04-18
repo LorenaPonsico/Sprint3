a0dd9042069820490c4d649b9f16a070e458f806
Lorena <lorena.poca@gmail.com>
2023-04-18 17:43:02 +0200

    aplicadas las promociones a los productos con descuento

--- js/shop.js
+++ js/shop.js
@@ -86,15 +86,15 @@ function buy(id) {
 
 // Exercise 2
 function cleanCart() {
-cartList = [] // reasigno variable para que se quede vacia de nuevo cuando aprieto boton Clean Cart
-console.log(cartList) //veo en la consola como el carrito (array cartList) esta vacio al apretar el boton
+    cartList = [] // reasigno variable para que se quede vacia de nuevo cuando aprieto boton Clean Cart
+    console.log(cartList) //veo en la consola como el carrito (array cartList) esta vacio al apretar el boton
 }
 
 // Exercise 3
 function calculateTotal() { // la funcion es llamada en open_modal()
     // Calculate total price of the cart using the "cartList" array
-for (let i = 0; i < cartList.length; i++) {
-       total += cartList[i].price // recorro el array cartList y voy sumando al total todos los precios
+    for (let i = 0; i < cartList.length; i++) {
+        total += cartList[i].price // recorro el array cartList y voy sumando al total todos los precios
     }
     console.log(total) // veo en la consola el total de todos los poductos seleccionados en cartList
     return total; // devuelvo la suma del total de la cartList
@@ -104,19 +104,19 @@ for (let i = 0; i < cartList.length; i++) {
 function generateCart() {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-for (let i = 0; i < cartList.length; i++) {
-    const product = cartList[i]
-    const productExist = cart.includes(product) // recorro el array cartList para saber si ya existe en la lista
+    for (let i = 0; i < cartList.length; i++) {
+        const product = cartList[i]
+        const productExist = cart.includes(product) // recorro el array cartList para saber si ya existe en la lista
 
-    if (!productExist) {
-        product.quantity = 1
-        cart.push(product) // si no existe pusheo la propiedad quantity en 1
-    }   
-    if (productExist) {
-        product.quantity++ // si existe añado 1 a la propiedad quantity
-    } 
-}
-console.log(cart) // veo en consola el array cart
+        if (!productExist) {
+            product.quantity = 1
+            cart.push(product) // si no existe pusheo la propiedad quantity en 1
+        }
+        if (productExist) {
+            product.quantity++ // si existe incremento en 1 la propiedad quantity
+        }
+    }
+    console.log(cart) // veo en consola el array cart
 }
 
 
@@ -124,6 +124,29 @@ console.log(cart) // veo en consola el array cart
 // Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    let subtotalWithDiscount = [] // array vacio donde con el primer bucle for voy añadiendo los precios totales con el descuento aplicado de cada producto
+    let total = 0 // creo una variable para sumar el total del array anterior
+
+    for (let i = 0; i < cart.length; i++) { // bucle for recorre el array cart (carrito compra final) y si cumple con las condiciones de descuento pushea la suma del total con el dto ya aplicado
+        const products = cart[i]
+
+        if (products.name === "cooking oil" && products.quantity > 2) {
+            subtotalWithDiscount.push(products.quantity * 10)
+        }
+        else if (products.name === "Instant cupcake mixture" && products.quantity > 9) {
+            subtotalWithDiscount.push(products.quantity * (10 / 3))
+        }
+        else {
+            subtotalWithDiscount.push(products.quantity * products.price) // los productos que no tienen descuento se pushean con el precio del producto base
+        }
+
+    }
+    for (let i = 0; i < subtotalWithDiscount.length; i++) { // recorro el array con todos los precios totales con dto aplicaodos y sin del carrito y los sumo para obtener un total final
+        total += subtotalWithDiscount[i]
+    }
+
+    console.log(total) //veo en consola el total de los productos del carrito con los dtos aplicados
+    return total
 }
 
 // Exercise 6
@@ -150,6 +173,7 @@ function removeFromCart(id) {
 function open_modal() {
     calculateTotal()
     generateCart()
+    applyPromotionsCart()
     console.log("Open Modal");
     printCart();
 }
