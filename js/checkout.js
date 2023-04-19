b09c523d0c92f8b95647f9dedeeb56d6c6d800e1
Lorena <lorena.poca@gmail.com>
2023-04-19 17:29:14 +0200

    validaciones formulario checkout

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,15 @@
 
 // Exercise 7
+
+function addInvalidClass(element, error) {
+	if (error) {
+		element.classList.add("is-invalid");
+	} else {
+		element.classList.remove("is-invalid");
+		element.classList.add("is-valid");
+	}
+};
+
 function validate() {
 	let error = 0;
 	// Get the input fields
@@ -10,25 +20,51 @@ function validate() {
 	let fPassword = document.getElementById("fPassword");
 	let fPhone = document.getElementById("fPhone");
 
-
-	// Get the error elements
-	let errorName = document.getElementById("errorName");
-	let errorEmail = document.getElementById("errorEmail");  
-	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == "" || fName.value.length < 3 || fName.value.match(/^[a-zA-Z]+$/){
-		errorName.innerHTML
+	if (fName.value == "" || fName.value.length < 3 || !fName.value.match(/^[a-zA-Z]+$/)) {
+		addInvalidClass(fName, true);
 		error++;
-	} // NO SE QUE ERROR HAY
-
-	if(fEmail.value == ""){
-		error++;
+	} else {
+		addInvalidClass(fName, false);
 	}
-	 
-	if(error>0){
+
+	if (fEmail.value == "" || fEmail.value.length < 3 || !fEmail.value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
+		addInvalidClass(fEmail, true);
+		error++;
+	} else {
+		addInvalidClass(fEmail, false);
+	}
+
+	if (fAddress == "" || fAddress.value.length < 3) {
+		addInvalidClass(fAddress, true);
+		error++;
+	} else {
+		addInvalidClass(fAddress, false);
+	}
+
+	if (fLastN == "" || fLastN.value.length < 3 || !fLastN.value.match(/^[a-zA-Z]+$/)) {
+		addInvalidClass(fLastN, true);
+		error++;
+	} else {
+		addInvalidClass(fLastN, false);
+	}
+	if (fPassword == "" || fPassword.value.length < 3 || !fPassword.value.match(/^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{3,8}$/)) {
+		addInvalidClass(fPassword, true);
+		error++;
+	} else {
+		addInvalidClass(fPassword, false);
+	}
+
+	if (fPhone == "" || fPhone.value.length < 3 || isNaN(fPhone.value)) {
+		addInvalidClass(fPhone, true);
+		error++;
+	} else {
+		addInvalidClass(fPhone, false);
+	}
+
+	if (error > 0) {
 		alert("Error");
-	}else{
+	} else {
 		alert("OK");
-	}
-
+	} 
 }
